8fb4c48959a1c4643e44f339aa189446dd3c4c8a
HappyCodingLover <moonshot191@gmail.com>
2023-10-12 19:46:58 -0700

    feat" update

--- frontend/src/pages/Pages/Register.jsx
+++ frontend/src/pages/Pages/Register.jsx
@@ -2,20 +2,31 @@ import React, { useState } from 'react';
 import { InputText, MyTextArea } from '../components/Input';
 import Button from '../components/Button';
 import { create } from '../api/company';
+import LoadingSpnner from '../components/Spinner';
 
 const Register = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [companyName, setCompanyName] = useState('');
   const [url, setUrl] = useState('');
   const [companyIntroduction, setCompanyIntroduction] = useState('');
   const [icp, setIcp] = useState('');
   const [others, setOthers] = useState('');
 
-  const handleRegister = (e) => {
-    create({ companyName, url, companyIntroduction, icp });
+  const handleRegister = async (e) => {
+    setIsLoading(true);
+    await create({ companyName, url, companyIntroduction, icp });
+    setIsLoading(false);
+    alert('Successfully Registered!')
+    setCompanyName('');
+    setUrl('');
+    setCompanyIntroduction('');
+    setIcp('');
+    setOthers('')
   };
 
   return (
     <div className="main-container">
+      {isLoading && <LoadingSpnner />}
       <InputText
         placeholder="Company Name"
         value={companyName}
